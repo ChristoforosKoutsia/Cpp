ab379052d9c44f157227d09f94c200241bd499d3
Christoforos Vasilis Koutsianoudis <48958980+Billkouts96@users.noreply.github.com>
2021-09-19 22:34:45 +0300

    Update geometry.h

--- Intro/Assignments/Introduction_to_Cpp_MIT/Assignment_3/geometry.h
+++ Intro/Assignments/Introduction_to_Cpp_MIT/Assignment_3/geometry.h
@@ -33,4 +33,38 @@ public:
     const Point* get(const int position) const;
 
 };
+
+class Polygon{
+/* Protected access modifier is similar to that of private access modifiers, the difference is that the class member declared as
+Protected are inaccessible outside the class but they can be accessed by any subclass(derived class) of that class.*/
+protected:
+    PointArray p_arr;
+    /*the variables declared as static are initialized only once as they are allocated space in separate static storage so,
+    the static variables in a class are shared by the objects. There can not be multiple copies of same static variables for
+    different objects.Also because of this reason static variables can not be initialized using constructors.
+    One instance of the variable across all instances of a class*/
+    static int n;
+public:
+    Polygon(const Point arr[],const int length);
+    Polygon(const PointArray p);
+    ~Polygon();
+    /*Sometimes implementation of all function cannot be provided in a base class because we don’t know the implementation.
+Such a class is called abstract class.We cannot create objects of abstract classes.
+A pure virtual function (or abstract function) in C++ is a virtual function for which we can have implementation,
+But we must override that function in the derived class, otherwise the derived class will also
+become abstract.A pure virtual function is declared by assigning 0 in declaration.*/
+virtual double area() = 0;
+static int getNumPolygons(){return n;}
+int getNumSides();
+const PointArray* getPoints() const;
+};
+
+class Rectangle: public Polygon{
+public:
+    Rectangle(const Point &x,const Point &y); //Pass by reference...we do not want to copy again objects
+    Rectangle(const int a,const int b,const int c,const int d);
+    virtual double area() const;
+
+};
 #endif // GEOMETRY_H_INCLUDED
+
