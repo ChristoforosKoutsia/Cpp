924acb72d2b93ff75a2845e342e4f479a714fd04
Christoforos Vasilis Koutsianoudis <48958980+Billkouts96@users.noreply.github.com>
2021-09-21 22:49:50 +0300

    Update geometry.cpp
    Added more features
    Implementation of main
    Bug fix: implementation of pure virtual area function

--- Intro/Assignments/Introduction_to_Cpp_MIT/Assignment_3/geometry.cpp
+++ Intro/Assignments/Introduction_to_Cpp_MIT/Assignment_3/geometry.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cmath>
 #include "geometry.h"
 #include <iostream>
 #include <math.h>
@@ -121,20 +122,18 @@ const Point *PointArray::get(const int position) const
 }
 
 //class Polygon: implementation
-Polygon::Polygon(const Point arr[],const int length)
+Polygon::Polygon(const Point arr[],const int length):p_arr(arr,length)
 {
-    p_arr=PointArray(arr,length);
     n++;
 }
 /*Implement a constructor that creates a polygon using the points in an existing PointArray that is passed as an argument.
 (For the purposes of this problem, you may assume that the order of the points in the PointArray traces out a convex polygon.)
  You should make sure your constructor avoids the unnecessary work of copying the entire existing PointArray each time it is called.*/
-Polygon::Polygon(const PointArray p)
+Polygon::Polygon(const PointArray &p):p_arr(p)
 {
-    p_arr=p;
-    n++;
+    ++n;
 }
-Polygon::~Polygon(){n--;}
+
 
 int Polygon::getNumSides()
 {
@@ -171,13 +170,54 @@ Rectangle::Rectangle(const Point &p1,const Point &p2):Polygon(updateConstructorP
 
 Rectangle::Rectangle(const int x1,const int x2, const int y1, const int y2):Polygon(updateConstructorPoints(Point(x1,y1),
                                                                                       Point(x2,y1),Point(x2,y2),Point(x1,y2)),4){}
-double Rectangle::area() const
+double Rectangle::area()
 {
     int length=p_arr.get(1)->getX()-p_arr.get(0)->getX();
     int width=p_arr.get(2)->getY()-p_arr.get(1)->getY();
-    return abs(length*width);
+    return abs((double)length*width);
 }
 
+/*Implementing Triangle class */
+Triangle::Triangle(const Point &p1,const Point &p2,const Point &p3):Polygon(updateConstructorPoints(p1,p2,p3),3){}
+double Triangle::area()
+{
+    //Calculate area using Heron's formula
+    double a=p_arr.get(1)->getX()-p_arr.get(0)->getX(); //base
 
-int main(){
+    //It is well known from basic vector theory that distance between two arbitary points it is given by :d=sqrt(pow(x2-x1)+pow(y2-y1))
+    double b=sqrt(pow(p_arr.get(2)->getX()-p_arr.get(1)->getX(),2)+pow((p_arr.get(2)->getY()-p_arr.get(1)->getY()),2));
+    double c=sqrt(pow((p_arr.get(2)->getX()-p_arr.get(0)->getX()),2)+pow((p_arr.get(2)->getY()-p_arr.get(0)->getY()),2));
+    double s=(a+b+c)/2;
+    return ((double)sqrt(s*(s-a)*(s-b)*(s-c)));
+
 }
+void printAttributes(Polygon *poly)
+{
+    cout<<"The area of the polygon is : "<<poly->area()<<endl;
+    cout<<"The points of the polygon are : \n";
+    //Store in point array the points
+    const PointArray *arr=poly->getPoints();
+    for (int i=0;i<arr->getSize();i++)
+    {
+        cout<<"("<<arr->get(i)->getX()<< ", " << arr->get(i)->getY()<<")"<<endl;
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+ cout << "Enter lower left and upper right coords of rectangle as four space separated integers: ";
+int llx, lly, urx, ury;
+cin >> llx >> lly >> urx >> ury;
+Point ll(llx, lly), ur(urx, ury);
+Rectangle r(ll, ur);
+printAttributes(&r);
+ cout << "Enter three coords of triangle as six space separated integers: ";
+ int x1, y1, x2, y2, x3, y3;
+ cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+Point a(x1, y1), b(x2, y2), c(x3, y3);
+Triangle t(a, b, c);
+printAttributes(&t);
+
+return 0;
+}
+
