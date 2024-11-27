8b00a069aa8f4968aaabf24c6aa3d0013e5f5a61
BillKouts96 <billkouts96@gmail.com>
2024-11-28 01:23:37 +0200

    Added new project

---
+++ Pico_Interview_challenge/interviewTest/src/build/CMakeFiles/WaveAnalyzer.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WaveAnalyzer.dir/main.cpp.obj"
+  "CMakeFiles/WaveAnalyzer.dir/main.cpp.obj.d"
+  "CMakeFiles/WaveAnalyzer.dir/signal/processing.cpp.obj"
+  "CMakeFiles/WaveAnalyzer.dir/signal/processing.cpp.obj.d"
+  "CMakeFiles/WaveAnalyzer.dir/sysio/csv_handler.cpp.obj"
+  "CMakeFiles/WaveAnalyzer.dir/sysio/csv_handler.cpp.obj.d"
+  "WaveAnalyzer.exe"
+  "WaveAnalyzer.exe.manifest"
+  "WaveAnalyzer.pdb"
+  "libWaveAnalyzer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WaveAnalyzer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Pico_Interview_challenge/interviewTest/src/build/CMakeFiles/WaveAnalyzer.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for WaveAnalyzer.
+# This may be replaced when dependencies are built.
