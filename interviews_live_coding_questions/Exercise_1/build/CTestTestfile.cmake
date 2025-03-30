090225690b94d493bde6dcab3cf0135b7985375a
BillKouts96 <billkouts96@gmail.com>
2025-03-30 18:20:39 +0100

    Deleted obsolete files

--- interviews_live_coding_questions/Exercise_1/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/billk/Documents/GitHub/Cpp/interviews_live_coding_questions/Exercise_1
-# Build directory: C:/Users/billk/Documents/GitHub/Cpp/interviews_live_coding_questions/Exercise_1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(AllTests "C:/Users/billk/Documents/GitHub/Cpp/interviews_live_coding_questions/Exercise_1/build/runTests.exe")
-set_tests_properties(AllTests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/billk/Documents/GitHub/Cpp/interviews_live_coding_questions/Exercise_1/CMakeLists.txt;34;add_test;C:/Users/billk/Documents/GitHub/Cpp/interviews_live_coding_questions/Exercise_1/CMakeLists.txt;0;")
